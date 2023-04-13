cd02218410d7bb27363f7ecb21ec8d5a968785a5
Tibo Pendino <tibo@starton.io>
2023-04-13 17:35:36 +0200

    feat(deps): update packages version

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -16,6 +16,7 @@ import { Field, Form, Formik } from "formik";
 import * as Yup from 'yup'
 import { ElementSvg } from "./common/svg/ElementSvg";
 import { OpenseaSvg } from "./common/svg/OpenseaSvg";
+import { Theme } from "@mui/system";
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,7 @@ import { OpenseaSvg } from "./common/svg/OpenseaSvg";
 */
 export interface BodyProps {}
 
+// TO MOVE
 export enum Network {
 	ETHEREUM_MAINNET = "ethereum-mainnet",
 	ETHEREUM_GOERLI = "ethereum-goerli",
@@ -38,6 +40,7 @@ export enum Network {
 	AVALANCHE_FUJI = "avalanche-fuji",
 }
 
+// TO MOVE
 export const explorerNetwork: Record<Network, string> = {
 	[Network.ETHEREUM_MAINNET]: "ethereum", // DONE
 	[Network.ETHEREUM_GOERLI]: "goerli", // DONE
@@ -80,9 +83,8 @@ export const Body: React.FC<BodyProps> = () => {
 		prompt: Yup.string().required(),
 	})
 
-	const theme = useTheme()
+	const theme: Theme = useTheme()
 	const [pictures, setPictures] = useState([])
-	// const [tx, setTx] = useState([])
 	const [isGenerationLoading, setGenerationLoading] = useState(false);
 	const [isGenerated, setGeneration] = useState(false);
 	const [isDeploymentLoading, setDeploymentLoading] = useState(false);
@@ -98,10 +100,11 @@ export const Body: React.FC<BodyProps> = () => {
 		return initials.join("");
 	}
 
-	function getCollectionUrl(collectionName: string): string {
-		const res = collectionName.replace(" ", '-').toLowerCase();
-		return res
-	}
+	// TO DELETE
+	// function getCollectionUrl(collectionName: string): string {
+	// 	const res = collectionName.replace(" ", '-').toLowerCase();
+	// 	return res
+	// }
 
 	const generatePictures = async (body: any) => {
 		setGenerationLoading(true);
@@ -152,52 +155,51 @@ export const Body: React.FC<BodyProps> = () => {
 		}).then(data => {
 			console.log('deployCollection --- ', data)
 			setSmartContractAddress(data.smartContractAddress)
-			// setTx(data)
 		}).catch((e) => { console.error(e)})
 	}
 
-	const handleCidSubmit = async (body: FormikInitialValues) => {
+	const handleGenerationSubmit = async (body: FormikInitialValues) => {
 		setBody(body)
 		console.log('BODY - ', body)
 		await generatePictures(body)
 	}
 
-	const selectOptions = useMemo<Array<StartonSelectOptionProps>>(() => {
-			return [
-				{
-					children: 'Ethereum - Mainnet',
-					value: Network.ETHEREUM_MAINNET,
-				},
-				{
-					children: 'Ethereum - Goerli',
-					value: Network.ETHEREUM_GOERLI,
-				},
-				{
-					children: 'BNB Chain - Mainnet',
-					value: Network.BINANCE_MAINNET,
-				},
-				{
-					children: 'BNB Chain - Testnet',
-					value: Network.BINANCE_TESTNET,
-				},
-				{
-					children: 'Polygon - Mainnet',
-					value: Network.POLYGON_MAINNET,
-				},
-				{
-					children: 'Polygon - Mumbai',
-					value: Network.POLYGON_MUMBAI,
-				},
-				{
-					children: 'Avalanche - Mainnet',
-					value: Network.AVALANCHE_MAINNET,
-				},
-				{
-					children: 'Avalanche - Fuji',
-					value: Network.AVALANCHE_FUJI,
-				},
-			]
-		}, [])
+	const selectOptions: StartonSelectOptionProps[] = useMemo<Array<StartonSelectOptionProps>>(() => {
+		return [
+			{
+				children: 'Ethereum - Mainnet',
+				value: Network.ETHEREUM_MAINNET,
+			},
+			{
+				children: 'Ethereum - Goerli',
+				value: Network.ETHEREUM_GOERLI,
+			},
+			{
+				children: 'BNB Chain - Mainnet',
+				value: Network.BINANCE_MAINNET,
+			},
+			{
+				children: 'BNB Chain - Testnet',
+				value: Network.BINANCE_TESTNET,
+			},
+			{
+				children: 'Polygon - Mainnet',
+				value: Network.POLYGON_MAINNET,
+			},
+			{
+				children: 'Polygon - Mumbai',
+				value: Network.POLYGON_MUMBAI,
+			},
+			{
+				children: 'Avalanche - Mainnet',
+				value: Network.AVALANCHE_MAINNET,
+			},
+			{
+				children: 'Avalanche - Fuji',
+				value: Network.AVALANCHE_FUJI,
+			},
+		]
+	}, [])
 
 	// prompt: 'random abstract majestic complex picture 8k'
 
@@ -205,6 +207,8 @@ export const Body: React.FC<BodyProps> = () => {
 	//--------------------------------------------------------------------------
 	return (
 		<Box maxWidth={1192} display="flex" flexDirection="column" gap={15} marginX="auto" marginTop={10} padding={5}>
+
+			{/*TITLE*/}
 			<Typography variant="h2" fontWeight={600}>
 				Create your NFT collection{' '}
 				<Typography color="secondary.main" variant="h2" component="span">
@@ -222,10 +226,11 @@ export const Body: React.FC<BodyProps> = () => {
 					</Typography>
 				</Box>
 
+				{/*FORM*/}
 				<Formik
 					initialValues={initialValues}
 					validationSchema={FormValidationSchema}
-					onSubmit={handleCidSubmit}>
+					onSubmit={handleGenerationSubmit}>
 					{/*{(formikProps) => (*/}
 						<Form>
 							{/*<Typography>{JSON.stringify(formikProps)}</Typography>*/}
@@ -321,6 +326,7 @@ export const Body: React.FC<BodyProps> = () => {
 
 			</Box>
 
+			{/*PICTURES*/}
 			{ isGenerated
 				? <Grid
 					container
@@ -345,6 +351,7 @@ export const Body: React.FC<BodyProps> = () => {
 				</Grid>
 				: null}
 
+				{/*DEPLOYMENT BUTTONS*/}
 				{isGenerated
 					? isDeployed
 						? <Box display="flex" flexDirection="row" gap={4} margin="auto" alignItems={'center'} flexWrap={'wrap'}>
